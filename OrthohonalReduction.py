07a4391d9abf46ebbffc54a97be88884b9e8acaf
Charlie Young <871706730@qq.com>
2016-11-06 12:37:45 +0800

    Update OrthohonalReduction.py

--- OrthohonalReduction.py
+++ OrthohonalReduction.py
@@ -2,47 +2,60 @@
 """
 Created on Sat Nov 05 21:17:19 2016
 
-@author: Charlie Young
+@author: Lin Lihui
 """
 
 
 from __future__ import division
 import numpy as np
 
-A = np.matrix([[0,-20,-14],
-              [3,27,-4],
-              [4,11,-2]])
-
-
 def norm(x,p=2):
     """p norm of a vector"""
     return np.sum(np.array(x)**p)**(1./p)
+
+def GivensReduction(A,precision=1e-8):
+    """orthohonal reduction of A using Givens reduction (plane rotation)"""
     
-m,n = A.shape
-P = {}
-
-i = 0; j = 0
-while j < min(m,n):
+    A = np.matrix(A)
+    m,n = A.shape
+    T = A.copy()
+    P = {}
     
-    P_ = np.identity(m)
-    for i in range(j+1,m):
-        Pij = np.identity(m)
+    i = 0; j = 0
+    while j < min(m,n):
+        
+        P_ = np.identity(m)
+        for i in range(j+1,m):
+            Pij = np.identity(m)
+    
+            c = float(T[j,j]/np.sqrt(T[j,j]**2+T[i,j]**2))
+            s = float(T[i,j]/np.sqrt(T[j,j]**2+T[i,j]**2))
+            Pij[j,j] = c
+            Pij[j,i] = s
+            Pij[i,j] = -s
+            Pij[i,i] = c
+            
+            T = np.dot(Pij,T)
+            P_ = np.dot(P_,Pij).T
+    
+        P[j] = P_
+        j += 1
+    
+    Q = np.identity(m)
+    for k in range(len(P))[::-1]:
+        Q = np.dot(Q,P[k])
+    
+    if precision:
+        Q[Q<precision] = 0
+        T[T<precision] = 0
+        
+    return Q,T
 
-        c = float(A[j,j]/np.sqrt(A[j,j]**2+A[i,j]**2))
-        s = float(A[i,j]/np.sqrt(A[j,j]**2+A[i,j]**2))
-        Pij[j,j] = c
-        Pij[j,i] = s
-        Pij[i,j] = -s
-        Pij[i,i] = c
-
-        A = np.dot(Pij,A)
-        P_ = np.dot(P_,Pij).T
-
-    P[j] = P_
-    j += 1
-
-Q = np.identity(m)
-for k in range(len(P))[::-1]:
-    Q = np.dot(Q,P[k])
-
-
+if __name__ == '__main__':
+    A = np.matrix([[0,-20,-14],
+              [3,27,-4],
+              [4,11,-2]])
+    
+    Q,T=GivensReduction(A)
+    print "Q: \n", Q
+    print "T: \n", T
